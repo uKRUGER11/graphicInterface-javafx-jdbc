3be8b17847100714149d0c86f3ecfc84259c4a9e
Gabriel Kruger <krugergabriel27@outlook.com>
2023-03-09 08:38:25 -0300

    C - Department created and notification for DepartmentList

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -85,6 +85,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
 			
